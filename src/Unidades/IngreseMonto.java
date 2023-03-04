4b8aadeccc6f3126457bae51e9f45849f0be29c2
Ivan <rivan181@gmail.com>
2023-03-04 17:01:30 -0300

    inicio del conversor

---
+++ src/Unidades/IngreseMonto.java
@@ -0,0 +1,15 @@
+package Unidades;
+
+public class IngreseMonto {
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
---
+++ src/Unidades/IngreseUnidad.java
@@ -0,0 +1,23 @@
+package Unidades;
+
+public class IngreseUnidad {
+	
+	private int dolarApeso = 1/380;
+	private int DollarPesoColombiano = 1/2500;
+	public int getDolarApeso() {
+		return dolarApeso;
+	}
+	public void setDolarApeso(int dolarApeso) {
+		this.dolarApeso = dolarApeso;
+	}
+	public int getDollarPesoColombiano() {
+		return DollarPesoColombiano;
+	}
+	public void setDollarPesoColombiano(int dollarPesoColombiano) {
+		DollarPesoColombiano = dollarPesoColombiano;
+	}
+
+	
+	
+	
+}
