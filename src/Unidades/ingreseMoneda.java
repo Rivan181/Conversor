6e12e6d7992f831adf66e7dae1ad72ecf5a50b5b
Ivan <rivan181@gmail.com>
2023-03-09 00:05:23 -0300

    casi finalizado las excepciones. queda uncluir valores enteros y continuar con el mensage desea continuar

--- src/Unidades/ingreseMoneda.java
+++ src/Unidades/ingreseMoneda.java
@@ -2,14 +2,18 @@ package Unidades;
 
 
 
-import java.util.Scanner;
+
+
+import java.awt.ActiveEvent;
 
 import javax.swing.JOptionPane;
 
+
+
 public class ingreseMoneda {
 	
-	static double unidades = 0;
-	
+	static String unidades;
+	static double unit;
 	
 	
 
@@ -17,13 +21,13 @@ public class ingreseMoneda {
 	public static void main(String[] args) {
 	
 	
-		
+	
 		String [] conversion={"Conversion de moneda", "Conversion de temperatura", "Conversion de longitud"};
 		String [] moneda={"Dolar a Peso Argentino", "Dolar a Peso Colombiano","Dolar a Euro", "Peso Argentino a Dolar", "Peso Colombiano a Dolar", "Euro a Dolar"};
 		String [] temperatura={"Celcius a Farenheit", "Farenheit a Celcius", "Celcius a Kelvin", "Kelvin a Celcius"};
 		String [] longitud={"Metros a Centimetros", "Centimetros a Metros", "Metros a Milimimetros", "Milimetros a Metros"};
 		
-		Scanner s = new Scanner(System.in);
+		
 		
 		
 		String opcion = JOptionPane.showInputDialog(null, "Seleccione la opcion de convercion", "Menu", JOptionPane.PLAIN_MESSAGE, null, conversion, null).toString();
@@ -39,118 +43,120 @@ public class ingreseMoneda {
 			
 			JOptionPane.showMessageDialog(null, "Seleccionó "+dos);
 			
-			if (dos.equals("Dolar a Peso Argentino")) {	
-				//try {
+			if (dos.equals("Dolar a Peso Argentino")) {					
 					
-				unidades = Double.parseDouble(JOptionPane.showInputDialog("introduzca el valor"));		
-				String unidades = s.nextLine();
-				while (!unidades.matches("[0-9]*")) {
-					while (!unidades.matches("[0-9]*")) {
-						JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
-						unidades = Double.parseDouble(JOptionPane.showInputDialog("introduzca el valor"));
-						
-
-					}
+				unidades = JOptionPane.showInputDialog("introduzca el valor");		
+				
+				while (!unidades.matches("^(?:\\+|-)?\\d+\\.\\d*$")) {
+					JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
+					unidades = JOptionPane.showInputDialog("introduzca el valor");
+				}
+				 unit = Double.parseDouble(unidades);
+					JOptionPane.showMessageDialog(null, "La cantidad es $"+ (unit * Conversion.dollarpesoArg) +" Dolares");
 				
 				
 				}
-				//} catch (NumberFormatException ex) {					
-						//JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
-					
-					
-							
-				JOptionPane.showMessageDialog(null, "La cantidad es $"+ (unidades * Conversion.dollarpesoArg) +" Dolares");
-					}
 			else if (dos.equals("Dolar a Peso Colombiano")) {
-				try {
-				unidades = Double.parseDouble(JOptionPane.showInputDialog("introduzca el valor"));		
-				} catch (NumberFormatException ex) {					
-					JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");					
+				unidades = JOptionPane.showInputDialog("introduzca el valor");		
+				
+				while (!unidades.matches("^(?:\\+|-)?\\d+\\.\\d*$")) {
+					JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
+					unidades = JOptionPane.showInputDialog("introduzca el valor");					
 				}
-				JOptionPane.showMessageDialog(null, "La cantidad es $"+ (unidades * Conversion.dollarApesoColom) +" Dolares");
+				unit = Double.parseDouble(unidades);
+				JOptionPane.showMessageDialog(null, "La cantidad es $"+ (unit * Conversion.dollarApesoColom) +" Dolares");
 				
 			}else if (dos.equals("Dolar a Euro")) {
-				try {
-					unidades = Double.parseDouble(JOptionPane.showInputDialog("introduzca el valor"));		
-					} catch (NumberFormatException ex) {					
-						JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");					
-					}
-					JOptionPane.showMessageDialog(null, "La cantidad es $"+ (unidades * Conversion.dollarAeuro)+ " Dolares");
-			}
+					unidades = JOptionPane.showInputDialog("introduzca el valor");		
+				
+				while (!unidades.matches("^(?:\\+|-)?\\d+\\.\\d*$")) {
+					JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
+					unidades = JOptionPane.showInputDialog("introduzca el valor");
+				}
+					unit = Double.parseDouble(unidades);
+					JOptionPane.showMessageDialog(null, "La cantidad es $"+ (unit * Conversion.dollarAeuro)+ " Dolares");
 			
+			}
 			//CONVERSION INVERSA
 			
 			if (dos.equals("Peso Argentino a Dolar")) {
-				try {
-					unidades = Double.parseDouble(JOptionPane.showInputDialog("introduzca el valor"));		
-					} catch (NumberFormatException ex) {					
-						JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");					
+				unidades = JOptionPane.showInputDialog("introduzca el valor");
+				while (!unidades.matches("^(?:\\+|-)?\\d+\\.\\d*$")) {
+					JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
+					unidades = JOptionPane.showInputDialog("introduzca el valor");					
 					}
-				JOptionPane.showMessageDialog(null, "La cantidad es $"+ (unidades * Conversion.pesoArgdolLar)+" Pesos Argentinos");
+				unit = Double.parseDouble(unidades);
+				JOptionPane.showMessageDialog(null, "La cantidad es $"+ (unit * Conversion.pesoArgdolLar)+" Pesos Argentinos");
 				
 				}else if (dos.equals("Peso Colombiano a Dolar")) {
-					try {
-						unidades = Double.parseDouble(JOptionPane.showInputDialog("introduzca el valor"));		
-						} catch (NumberFormatException ex) {					
-							JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");					
+					unidades = JOptionPane.showInputDialog("introduzca el valor");
+					while (!unidades.matches("^(?:\\+|-)?\\d+\\.\\d*$")) {
+						JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
+						unidades = JOptionPane.showInputDialog("introduzca el valor");					
 						}
-					JOptionPane.showMessageDialog(null, "La cantidad es $"+ (unidades * Conversion.ApesoColomdollar)+" Pesos Colombianos");
-					
+					unit = Double.parseDouble(unidades);
+					JOptionPane.showMessageDialog(null, "La cantidad es $"+ (unit * Conversion.ApesoColomdollar)+" Pesos Colombianos");
+			
 				}else if (dos.equals("Euro a Dolar")) {
-					try {
-						unidades = Double.parseDouble(JOptionPane.showInputDialog("introduzca el valor"));		
-						} catch (NumberFormatException ex) {					
-							JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");					
+					unidades = JOptionPane.showInputDialog("introduzca el valor");
+					while (!unidades.matches("^(?:\\+|-)?\\d+\\.\\d*$")) {
+						JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
+						unidades = JOptionPane.showInputDialog("introduzca el valor");					
 						}
-						JOptionPane.showMessageDialog(null, "La cantidad es $"+ (unidades * Conversion.Aeurodollar) + " Euros");
+					unit = Double.parseDouble(unidades);
+						JOptionPane.showMessageDialog(null, "La cantidad es $"+ (unit * Conversion.Aeurodollar) + " Euros");
 				}
 		}
-			
+		
 //CONVERSION TEMPERATURA
-			
+	
 			//String [] temperatura={"Celcius a Farenheit", "Farenheit a Celcius", "Celcius a Kelvin", "Kelvin a Celcius"};
 			
-		else if  (opcion.equals("Conversion de temperatura")) {
+				else if  (opcion.equals("Conversion de temperatura")) {
 			
 				String tres = JOptionPane.showInputDialog(null, "Seleccione la opcion de convercion", "Temperatura", JOptionPane.PLAIN_MESSAGE, null, temperatura, null).toString();
 			
 				JOptionPane.showMessageDialog(null, "Seleccionó "+ tres);
 				if (tres.equals("Celcius a Farenheit")) {
-					try {
-						unidades = Double.parseDouble(JOptionPane.showInputDialog("introduzca el valor"));		
-						} catch (NumberFormatException ex) {					
-							JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");					
+					unidades = JOptionPane.showInputDialog("introduzca el valor");
+					while (!unidades.matches("^(?:\\+|-)?\\d+\\.\\d*$")) {
+						JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
+						unidades = JOptionPane.showInputDialog("introduzca el valor");					
 						}
-					JOptionPane.showMessageDialog(null, "Son "+ ((unidades * (9/5)+(32)))+" grados Farenhait");
+					unit = Double.parseDouble(unidades);
+					JOptionPane.showMessageDialog(null, "Son "+ ((unit * (9/5)+(32)))+" grados Farenhait");
 					
 				}else if (tres.equals("Farenheit a Celcius")) {
-					try {
-						unidades = Double.parseDouble(JOptionPane.showInputDialog("introduzca el valor"));		
-						} catch (NumberFormatException ex) {					
-							JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");					
-						}							
-					JOptionPane.showMessageDialog(null, "Son "+ ((unidades -(32))*(5/9))+" grados Farenhait");
+					unidades = JOptionPane.showInputDialog("introduzca el valor");
+					while (!unidades.matches("^(?:\\+|-)?\\d+\\.\\d*$")) {
+						JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
+						unidades = JOptionPane.showInputDialog("introduzca el valor");					
+						}		
+					unit = Double.parseDouble(unidades);
+					JOptionPane.showMessageDialog(null, "Son "+ ((unit -(32))*(5/9))+" grados Farenhait");
 					
 				}else if (tres.equals("Celcius a Kelvin")) {
-					try {
-						unidades = Double.parseDouble(JOptionPane.showInputDialog("introduzca el valor"));		
-						} catch (NumberFormatException ex) {					
-							JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");					
-						}							
-					JOptionPane.showMessageDialog(null, "Son "+ (unidades + Conversion.celAkelvin)+" grados Kelvin");
+					unidades = JOptionPane.showInputDialog("introduzca el valor");
+					while (!unidades.matches("^(?:\\+|-)?\\d+\\.\\d*$")) {
+						JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
+						unidades = JOptionPane.showInputDialog("introduzca el valor");					
+						}	
+					unit = Double.parseDouble(unidades);
+					JOptionPane.showMessageDialog(null, "Son "+ (unit + Conversion.celAkelvin)+" grados Kelvin");
 
 				}else if (tres.equals("Kelvin a Celcius")) {
-					try {
-						unidades = Double.parseDouble(JOptionPane.showInputDialog("introduzca el valor"));		
-						} catch (NumberFormatException ex) {					
-							JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");					
-						}							
-					JOptionPane.showMessageDialog(null, "Son "+ (unidades + Conversion.kelvinAcel)+" grados Celcius");
+					unidades = JOptionPane.showInputDialog("introduzca el valor");
+					while (!unidades.matches("^(?:\\+|-)?\\d+\\.\\d*$")) {
+						JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
+						unidades = JOptionPane.showInputDialog("introduzca el valor");					
+						}
+					unit = Double.parseDouble(unidades);
+					JOptionPane.showMessageDialog(null, "Son "+ (unit + Conversion.kelvinAcel)+" grados Celcius");
 				}
 		
-			
+				
 		
-		}		
+				}
 		
 //CONVERSION DE LONGITUD		
 		
@@ -164,41 +170,51 @@ public class ingreseMoneda {
 		
 			
 				if(cuatro.equals("Metros a Centimetros")) {
-					try {
-						unidades = Double.parseDouble(JOptionPane.showInputDialog("introduzca el valor"));		
-						} catch (NumberFormatException ex) {					
-							JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");					
-						}							
-					JOptionPane.showMessageDialog(null, "Son "+ (unidades *Conversion.metrosAcentimetros)+" centimetros");					
+					unidades = JOptionPane.showInputDialog("introduzca el valor");
+					while (!unidades.matches("^(?:\\+|-)?\\d+\\.\\d*$")) {
+						JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
+						unidades = JOptionPane.showInputDialog("introduzca el valor");					
+						}	
+					unit = Double.parseDouble(unidades);
+					JOptionPane.showMessageDialog(null, "Son "+ (unit *Conversion.metrosAcentimetros)+" centimetros");					
 					
 				}else if(cuatro.equals("Centimetros a Metros")) {
-					try {
-						unidades = Double.parseDouble(JOptionPane.showInputDialog("introduzca el valor"));		
-						} catch (NumberFormatException ex) {					
-							JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");					
-						}							
-					JOptionPane.showMessageDialog(null, "Son "+ (unidades /Conversion.centimetrosAmetros)+" metros");
+					unidades = JOptionPane.showInputDialog("introduzca el valor");
+					while (!unidades.matches("^(?:\\+|-)?\\d+\\.\\d*$")) {
+						JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
+						unidades = JOptionPane.showInputDialog("introduzca el valor");					
+						}	
+					unit = Double.parseDouble(unidades);
+					JOptionPane.showMessageDialog(null, "Son "+ (unit /Conversion.centimetrosAmetros)+" metros");
 					
 				}else if(cuatro.equals("Metros a Milimimetros")) {
-					try {
-						unidades = Double.parseDouble(JOptionPane.showInputDialog("introduzca el valor"));		
-						} catch (NumberFormatException ex) {					
-							JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");					
-						}							
-					JOptionPane.showMessageDialog(null, "Son "+ (unidades /Conversion.metrosAmilimetros)+" milimetros");
+					unidades = JOptionPane.showInputDialog("introduzca el valor");
+					while (!unidades.matches("^(?:\\+|-)?\\d+\\.\\d*$")) {
+						JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
+						unidades = JOptionPane.showInputDialog("introduzca el valor");						
+						}		
+					unit = Double.parseDouble(unidades);
+					JOptionPane.showMessageDialog(null, "Son "+ (unit /Conversion.metrosAmilimetros)+" milimetros");
 				}else if(cuatro.equals("Milimetros a Metros")) {
-					try {
-						unidades = Double.parseDouble(JOptionPane.showInputDialog("introduzca el valor"));		
-						} catch (NumberFormatException ex) {					
-							JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");					
-						}							
-					JOptionPane.showMessageDialog(null, "Son "+ (unidades /Conversion.milimetrosAmetros)+" metros");
+					unidades = JOptionPane.showInputDialog("introduzca el valor");
+					while (!unidades.matches("^(?:\\+|-)?\\d+\\.\\d*$")) {
+						JOptionPane.showMessageDialog(null, "Solo se acepta un valor numerico");
+						unidades = JOptionPane.showInputDialog("introduzca el valor");					
+						}
+					unit = Double.parseDouble(unidades);
+					JOptionPane.showMessageDialog(null, "Son "+ (unit /Conversion.milimetrosAmetros)+" metros");
 				}
+		}
+		
+		JOptionPane.showMessageDialog(null, "Finalizo el programa");
+		
 		
 		
 		}
-		JOptionPane.showMessageDialog(null, "Finalizo el programa");
-	}
+		
+	
+		
+	
 		
 }
 	
